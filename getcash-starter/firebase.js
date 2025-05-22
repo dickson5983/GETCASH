df1fc2fe3d3a88cc3a91bd507146cc054e9b3625
dickson5983 <dicksonmutinda06@gmail.com>
2025-05-22 11:16:37 +0300

    Add files via upload

---
+++ getcash-starter/firebase.js
@@ -0,0 +1,11 @@
+
+// Firebase config placeholder (use your real config from Firebase Console)
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
